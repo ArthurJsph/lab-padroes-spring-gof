b57b0db885f46296ea4d28499006ed6465e76de1
Arthur José <jarthurben10@gmail.com>
2024-07-29 16:50:28 -0300

    projeto usando spring

---
+++ src/main/java/dio/projeto/springgof/service/ClienteService.java
@@ -0,0 +1,17 @@
+
+package dio.projeto.springgof.service;
+import one.digitalinnovation.gof.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
